b3cb00222ceb6dfbf491a1fa60d007bc4ce81727
Zeke Davis <edavis@javafoundry.com>
2023-04-04 20:43:22 -0400

    runner finds action of each line

--- tankPrograming/runner.py
+++ tankPrograming/runner.py
@@ -3,8 +3,24 @@ import sys
 filename = sys.argv[1]
 f = open(filename, "r")
 commands = f.readlines()
+
+def findAction(x):
+    if x == "w":
+        return "forward"
+    elif x == "s":
+        return "backward"
+    elif x == "a":
+        return "turn left"
+    elif x == "d":
+        return "turn right"
+
+
 for command in commands:
     if command[0] == "#":
         print(command.strip())
+    
     else:
-        pass
+        action = findAction(command[0])
+        print(action)
+        #we could use <print(findAction(command[0]))>, but i have later use for action
+        
