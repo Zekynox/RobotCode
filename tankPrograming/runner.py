a9e7ed65f5143e692a9b14715a39a4d4942fbf29
Zeke Davis <edavis@javafoundry.com>
2023-05-10 15:18:31 -0400

    fixed up runner

--- tankPrograming/runner.py
+++ tankPrograming/runner.py
@@ -1,9 +1,73 @@
 # reads and runs tank runner format
 import sys
+import time
+import RPi.GPIO as GPIO
+
 filename = sys.argv[1]
 f = open(filename, "r")
 commands = f.readlines()
 
+IN1 = 20
+IN2 = 21
+IN3 = 19
+IN4 = 26
+ENA = 16
+ENB = 13
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+def motor_init():
+    global pwm_ENA
+    global pwm_ENB
+    GPIO.setup(ENA,GPIO.OUT,initial=GPIO.HIGH)
+    GPIO.setup(IN1,GPIO.OUT,initial=GPIO.LOW)
+    GPIO.setup(IN2,GPIO.OUT,initial=GPIO.LOW)
+    GPIO.setup(ENB,GPIO.OUT,initial=GPIO.HIGH)
+    GPIO.setup(IN3,GPIO.OUT,initial=GPIO.LOW)
+    GPIO.setup(IN4,GPIO.OUT,initial=GPIO.LOW)
+    #Set the PWM pin and frequency is 2000hz
+    pwm_ENA = GPIO.PWM(ENA, 2000)
+    pwm_ENB = GPIO.PWM(ENB, 2000)
+    pwm_ENA.start(0)
+    pwm_ENB.start(0)
+
+def w(speed, seconds): 
+    GPIO.output(IN1, GPIO.HIGH)
+    GPIO.output(IN2, GPIO.LOW)
+    GPIO.output(IN3, GPIO.HIGH)
+    GPIO.output(IN4, GPIO.LOW)
+    pwm_ENA.ChangeDutyCycle(speed)
+    pwm_ENB.ChangeDutyCycle(speed)
+    time.sleep(seconds)
+
+def s(speed, seconds): 
+    GPIO.output(IN1, GPIO.LOW)
+    GPIO.output(IN2, GPIO.HIGH)
+    GPIO.output(IN3, GPIO.LOW)
+    GPIO.output(IN4, GPIO.HIGH)
+    pwm_ENA.ChangeDutyCycle(speed)
+    pwm_ENB.ChangeDutyCycle(speed)
+    time.sleep(seconds)
+    
+def d(speed, seconds): 
+    GPIO.output(IN1, GPIO.HIGH)
+    GPIO.output(IN2, GPIO.LOW)
+    GPIO.output(IN3, GPIO.LOW)
+    GPIO.output(IN4, GPIO.HIGH)
+    pwm_ENA.ChangeDutyCycle(speed)
+    pwm_ENB.ChangeDutyCycle(speed)
+    time.sleep(seconds)
+
+def a(speed, seconds): 
+    GPIO.output(IN1, GPIO.LOW)
+    GPIO.output(IN2, GPIO.HIGH)
+    GPIO.output(IN3, GPIO.HIGH)
+    GPIO.output(IN4, GPIO.LOW)
+    pwm_ENA.ChangeDutyCycle(speed)
+    pwm_ENB.ChangeDutyCycle(speed)
+    time.sleep(seconds)
+    
 def findAction(x):
     if x == "w":
         return "forward"
@@ -14,30 +78,22 @@ def findAction(x):
     elif x == "d":
         return "turn right"
 
+motor_init()
 
 for command in commands:
     if command[0] == "#":
         print(command.strip())
+        
+    elif command[0] == "\n":
+        pass
     
     else:
-        action = findAction(command[0])
-        speed = command[2]
-        timeAt = 4
-        
-        if command[3] != " ":
-            speed += command[3]
-            timeAt += 1
-            if command[4] != " ":
-                speed += command[4]
-                timeAt += 1
-                
-        time = command[timeAt]
-        timeAt += 1
-        
-        while command[timeAt] != " ":
-             timeAt += 1
-             time += command[timeAt]
-            
-        print(action + " at " + speed + " for " + time)
-        
+        a = command.split()
+        action = findAction(a[0])
+        speed = a[1]
+        seconds = a[2]
+        print(f"{action} at {speed} for {seconds}")
+        cmd = f"{a[0]}({speed}, {seconds})"
+        print(cmd)
+        eval(cmd)
         
